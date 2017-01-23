b8529b4f290c4c92615105731573ec7b276b21dd
Gabor Meszaros <gabor-meszaros@users.noreply.github.com>
2017-01-23 15:38:29 +0100

    Finalize adding RecyclerView base structure

--- app/src/main/java/name/meszaros/gabor/popularmovies/BrowseMoviesActivity.java
+++ app/src/main/java/name/meszaros/gabor/popularmovies/BrowseMoviesActivity.java
@@ -1,13 +1,30 @@
 package name.meszaros.gabor.popularmovies;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class BrowseMoviesActivity extends AppCompatActivity {
 
+    private RecyclerView mMoviesRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Context context = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse_movies);
+
+        mMoviesRecyclerView = (RecyclerView) findViewById( R.id.rv_movies );
+
+        final MoviesAdapter adapter = new MoviesAdapter();
+
+        mMoviesRecyclerView.setAdapter(adapter);
+
+        final int spanCount = 2;
+        final GridLayoutManager layoutManager = new GridLayoutManager(context, spanCount);
+
+        mMoviesRecyclerView.setLayoutManager(layoutManager);
     }
 }

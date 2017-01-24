aa1ea172af70859740ad75972cb26645ad4f7f31
Gabor Meszaros <gabor-meszaros@users.noreply.github.com>
2017-01-24 13:07:22 +0100

    Prepare the adapter to make in updateable

--- app/src/main/java/name/meszaros/gabor/popularmovies/MoviesAdapter.java
+++ app/src/main/java/name/meszaros/gabor/popularmovies/MoviesAdapter.java
@@ -14,17 +14,11 @@ import com.squareup.picasso.Picasso;
  */
 public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder> {
 
-    private static final Movie[] sDummyMovies = {
-            new Movie("Rogue One", "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SY1000_SX675_AL_.jpg"),
-            new Movie("The Dark Knight", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg"),
-            new Movie("Pulp Fiction", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTkxMTA5OTAzMl5BMl5BanBnXkFtZTgwNjA5MDc3NjE@._V1_SY1000_CR0,0,673,1000_AL_.jpg"),
-            new Movie("Passangers", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk4MjU3MDIzOF5BMl5BanBnXkFtZTgwMjM2MzY2MDI@._V1_SY1000_CR0,0,675,1000_AL_.jpg"),
-            new Movie("Forrest Gump", "https://images-na.ssl-images-amazon.com/images/M/MV5BYThjM2MwZGMtMzg3Ny00NGRkLWE4M2EtYTBiNWMzOTY0YTI4XkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SY1000_CR0,0,757,1000_AL_.jpg"),
-            new Movie("Moana", "https://images-na.ssl-images-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_SY1000_CR0,0,674,1000_AL_.jpg"),
-            new Movie("Fight Club", "https://images-na.ssl-images-amazon.com/images/M/MV5BMzc1YmU2ZjEtYWIwMC00ZjM3LWI0NTctMDVlNGQ3YmYwMzE5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY999_CR0,0,704,999_AL_.jpg"),
-            new Movie("Frozen", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ1MjQwMTE5OF5BMl5BanBnXkFtZTgwNjk3MTcyMDE@._V1_SY1000_CR0,0,674,1000_AL_.jpg"),
-            new Movie("Arrival", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTExMzU0ODcxNDheQTJeQWpwZ15BbWU4MDE1OTI4MzAy._V1_SY1000_CR0,0,640,1000_AL_.jpg")
-    };
+    private Movie[] mMovies;
+
+    public MoviesAdapter(Movie[] movies) {
+        mMovies = movies;
+    }
 
     @Override
     public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -45,12 +39,14 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
 
     @Override
     public void onBindViewHolder(MovieViewHolder holder, int position) {
-        holder.bind(position);
+        final Movie movie = mMovies[position];
+        holder.bind(movie);
     }
 
     @Override
     public int getItemCount() {
-        return sDummyMovies.length;
+        if (null == mMovies) return 0;
+        return mMovies.length;
     }
 
     /**
@@ -58,7 +54,7 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
      */
     public static class MovieViewHolder extends RecyclerView.ViewHolder {
 
-        public ImageView moviePoster;
+        private ImageView moviePoster;
 
         private final Context mContext;
 
@@ -69,9 +65,8 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
             moviePoster = (ImageView) itemView.findViewById(R.id.tv_movie_item_poster);
         }
 
-        public void bind(int position) {
-            final Movie movieToBind = sDummyMovies[position];
-            final String posterLinkToBind = movieToBind.getPosterLink();
+        public void bind(Movie movie) {
+            final String posterLinkToBind = movie.getPosterLink();
             Picasso.with(mContext).load(posterLinkToBind).into(moviePoster);
         }
     }

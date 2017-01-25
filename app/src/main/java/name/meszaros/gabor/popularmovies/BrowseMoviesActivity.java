fb082037f335684e74dd211b35f0f8cd6547a5b2
Gabor Meszaros <gabor-meszaros@users.noreply.github.com>
2017-01-25 12:35:31 +0100

    Add menu to prepare for different movie lists

--- app/src/main/java/name/meszaros/gabor/popularmovies/BrowseMoviesActivity.java
+++ app/src/main/java/name/meszaros/gabor/popularmovies/BrowseMoviesActivity.java
@@ -5,8 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class BrowseMoviesActivity extends AppCompatActivity implements FetchMoviesTask.Listener {
+
+    private static final String LOG_TAG = BrowseMoviesActivity.class.getSimpleName();
 
     private RecyclerView mMoviesRecyclerView;
     private MoviesAdapter mAdapter;
@@ -29,6 +36,29 @@ public class BrowseMoviesActivity extends AppCompatActivity implements FetchMovi
         mMoviesRecyclerView.setLayoutManager(layoutManager);
 
         new FetchMoviesTask(this).execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        final MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_browse_movies, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final int itemId = item.getItemId();
+        switch (itemId) {
+            case R.id.action_switch_popular:
+                Toast.makeText(this, "Switch popular selected.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_switch_highest_rated:
+                Toast.makeText(this, "Switch highest rated selected.", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Log.w(LOG_TAG, "Menu selection is not handled. ItemId: " + itemId);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
